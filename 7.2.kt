48eabbbf6d997272d14f2c3ce9bbbf3a2db3211d
seany <shhekwu@gmail.com>
2024-10-01 02:19:06 +0900

    Add files via upload

---
+++ 7.2.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Введите слово:")
+    val word = readLine()!!
+
+    if (isPalindrome(word)) {
+        println("Слово \"$word\" является палиндромом.")
+    } else {
+        println("Слово \"$word\" не является палиндромом.")
+    }
+}
+
+fun isPalindrome(word: String): Boolean {
+    val cleanedWord = word.replace("\\s".toRegex(), "").toLowerCase()
+    return cleanedWord == cleanedWord.reversed()
+}
